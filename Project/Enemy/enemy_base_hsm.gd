35063b56af3f8d606efde7ef12b1d132fba4fccb
Flavore669 <dirkosh2@gmail.com>
2024-12-15 15:11:59 -0600

    got rid of unnecessary code

--- Project/Enemy/enemy_base_hsm.gd
+++ Project/Enemy/enemy_base_hsm.gd
@@ -4,68 +4,36 @@ class_name EnemyHSM
 #FIXME: This also contributes to crashes
 #FIXME: Enemy doesn't update if patrol point is placed on top of them
 
-var detection_ui : String = "UI/PlayerUI/Detection"
 
-@onready var queue_free_timer = Timer.new()
-#@onready var vision_area: Area2D = $Rig/Left/FlashLightVision/VisionArea
-@onready var dash_timer = Timer.new()
-@onready var suspicious_timer = Timer.new()
 @onready var stay_still_timer = Timer.new()
-@onready var look_timer: Timer = $Timers/LookTimer
-@onready var look_around_timer: Timer = $Timers/LookAroundTimer
+
 
 #@onready var flashlight_vision_cone: Polygon2D = $Rig/Left/FlashLightVision/VisionCone
 
 @onready var nav: NavigationAgent2D = $NavigationAgent2D
-
-@onready var footstep_sounds_tile: AudioStreamPlayer2D = $Audio/FootstepSoundManager/FootstepSoundsTile
-@onready var footstep_sounds_dirt: AudioStreamPlayer2D = $Audio/FootstepSoundManager/FootstepSoundsDirt
-@onready var footstep_timer: Timer = $Audio/FootstepSoundManager/FootstepTimer
-
-var current_footsteps : AudioStreamPlayer2D
-
-@export var health_packs := 0
-@export var backtrack : bool = false
-@export_category("Combat Logic")
-@export var to_action_wait_time : float = 3
-@export var follow_up_points : int = 5
-
-#Movement Variables
-@export var chill_speed := 150
-
-@export_category("Orbit State Variables")
-@export var angle := 30
-@export var radius := 175
-@export var orbit_speed := 100
 
 @export_category("Base")
 @export var hsm : LimboHSM #This stays as export var
 
 #Maybe give each point an export var for how much time the enemy idles
 @export var patrol_points : Array[PatrolPoints]= []
+@export var backtrack := false
 var next_point : PatrolPoints
 var counter : int = 0
 var starting_global_pos : Vector2
 
 @export_category("Stats")
 @export var patrol_speed := 100
-@export var sus_speed := 200
-@export var chase_speed := 250
 
-var player_in_flashlight := false
-var sus_pos : Vector2
+
 var rotation_speed := 5.0
-var action_speed := 35
-var vision_color : Color
 var direction : Vector2
 var state_list = []
 #HACK: This global variable can be replaced by the nav's target
 var target_pos : Vector2 = global_position
-var dash_velocity : Vector2
+
 var current_speed : int
-var is_parrying : bool = false
-var in_action : bool = false
-var not_dead := true
+
 
 func _ready() -> void:
 	intialize_nodes()
@@ -80,7 +48,7 @@ func _ready() -> void:
 	
 	
 	next_point = patrol_points[0]
-	current_footsteps = footstep_sounds_tile
+	
 
 func intialize_nodes():
 	for child in get_children():
@@ -93,17 +61,6 @@ func intialize_nodes():
 	
 	stay_still_timer.one_shot = true
 	add_child(stay_still_timer)
-	
-	suspicious_timer.one_shot = true
-	suspicious_timer.wait_time = 10
-	add_child(suspicious_timer)
-	
-	dash_timer.one_shot = true
-	add_child(dash_timer)
-	
-	queue_free_timer.one_shot = true
-	queue_free_timer.wait_time = 30
-	add_child(queue_free_timer)
 
 
 #region Bug Should Occur here
@@ -111,7 +68,6 @@ func intialize_nodes():
 #Idle State
 func _idle_ready() -> void:
 	#print("idle")
-	footstep_timer.wait_time = 1
 	current_speed = patrol_speed
 
 func move_towards_point():
